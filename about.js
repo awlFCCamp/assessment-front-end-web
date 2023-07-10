e8f1012b097ee024972f216c4e616b488c1a60f6
Angela Lee <angelalee@Angelas-iMac.local>
2023-07-09 22:10:56 -0500

    clean up

--- about.js
+++ about.js
@@ -1,17 +1,8 @@
 console.log("hello world");
-const selectedRadioButton = document.querySelector(
-  'input[name="where"]:checked'
-);
-
-// Check if a radio button is selected
-if (selectedRadioButton) {
-  const selectedValue = selectedRadioButton.value;
-}
 
 function handleSubmit(evt) {
   evt.preventDefault();
   alert(` Your form has been submitted successfully!`);
-  //console.log('form submit');
 }
 
 let form = document.querySelector("#contact");
